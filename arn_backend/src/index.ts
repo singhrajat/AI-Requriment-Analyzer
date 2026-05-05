e35e955a243662381535f6ff3a13c2a155169941
singhrajat <singhrjt07@gmail.com>
2026-05-05 22:33:45 +0530

    fixes cross origin

--- arn_backend/src/index.ts
+++ arn_backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "http://AI-Requriment-Analyzer.railway.internal:3000",
+      "https://ai-requirement-analyzer-prod.up.railway.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
